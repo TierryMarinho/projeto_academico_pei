ed2652e358d38c9b841a32fc0c3315ccd5c1e25b
TierryMarinho <TierryMarinho@users.noreply.github.com>
2026-06-09 14:20:46 -0300

    feat: implement WhatsApp contact form submission and add target blank to header link

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Lógica para enviar formulário de contato pelo WhatsApp
+document.getElementById('btn-whatsapp').addEventListener('click', function () {
+    var mensagem = document.getElementById('mensagem').value;
+
+    var texto = "";
+    if (mensagem) {
+        texto += mensagem;
+    }
+
+    var numeroWhatsApp = "5527992510305";
+    var url = "https://api.whatsapp.com/send?phone=" + numeroWhatsApp + "&text=" + encodeURIComponent(texto);
+
+    window.open(url, '_blank');
+});
